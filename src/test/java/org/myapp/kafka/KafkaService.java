6db12a3dc6dbb873cb6415f76ca8074805ed7459
RusAli <mr.al1mov.ruslan@gmail.com>
2025-02-04 23:54:40 +0300

    kafka

--- src/test/java/org/myapp/kafka/KafkaService.java
+++ src/test/java/org/myapp/kafka/KafkaService.java
@@ -3,6 +3,7 @@ package org.myapp.kafka;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.protocol.types.Field;
 import org.awaitility.Awaitility;
@@ -16,6 +17,7 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
+@Slf4j
 @AllArgsConstructor
 public class KafkaService {
 
@@ -25,7 +27,10 @@ public class KafkaService {
     @Step("Send message to kafka")
     public void sendMessage(String message) {
         CompletableFuture<SendResult<String, String>> send = kafkaTemplate.send("test-topic", UUID.randomUUID().toString(), message);
-        Awaitility.await().atMost(10, TimeUnit.SECONDS).until(send::isDone);
+        Awaitility.await("Await sending message")
+                .logging(log::info)
+                .atMost(10, TimeUnit.SECONDS)
+                .until(send::isDone);
     }
 
     @KafkaListener(topics = "test-topic", groupId = "test")
@@ -38,7 +43,8 @@ public class KafkaService {
 
     @Step("Receive message")
     public ConsumerRecord<String, String> getRecord(){
-        Awaitility.await()
+        Awaitility.await("Await receiving message")
+                .logging(log::info)
                 .atMost(10,TimeUnit.SECONDS)
                 .until(() -> !consumerRecords.isEmpty());
         return consumerRecords.get(0);
